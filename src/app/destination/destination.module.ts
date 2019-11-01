0d06fe1e9b799443a697c395470da1b48ed2189c
bngo42 <bngo@student.42.fr>
2019-11-01 15:18:35 +0100

    adding activity component/resolver to module

--- src/app/destination/destination.module.ts
+++ src/app/destination/destination.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { DestinationService } from './destination.service';
 import { DestinationRoutingModule } from './destination.router';
 import { DestinationComponent } from './destination.component';
-import { DestinationThumbnailComponent, DestinationGalleryComponent } from './components';
+import { DestinationThumbnailComponent, DestinationGalleryComponent, DestinationActivityComponent } from './components';
 import { DestinationResolver } from './destination.resolver';
+import { DestinationActivityResolver } from './destination.activity.resolver';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiModule } from '../api';
@@ -18,11 +19,13 @@ import { ApiModule } from '../api';
 	providers: [
 		DestinationService,
 		DestinationResolver,
+		DestinationActivityResolver
 	],
 	declarations: [
 		DestinationComponent,
 		DestinationThumbnailComponent,
 		DestinationGalleryComponent,
+		DestinationActivityComponent,
 	],
 	exports: [
 		DestinationThumbnailComponent,
